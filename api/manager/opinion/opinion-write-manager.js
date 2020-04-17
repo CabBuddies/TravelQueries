811bec9ab79cb6fa357b9477afbd6907012e15e0
Nihal Konda <nihal.konda@sjsu.edu>
2020-04-17 02:54:18 -0700

    Opinion Write Manager fixed

--- api/manager/opinion/opinion-write-manager.js
+++ api/manager/opinion/opinion-write-manager.js
@@ -73,17 +73,12 @@ async function saveOpinion(opinion){
 function invalidFormat(opinion,full){
     
     console.log(opinion)
-
     console.log('checking for invalid format in opinion')
 
-    if(full){
-        if(opinion._id===undefined)
-            return true
-    }else{
-        if(opinion._id!==undefined)
-            return true
-    }
 
+    console.log(full)
+    console.log((opinion._id!==undefined))
+    
     if(full === (opinion._id!==undefined))
         return true
 
